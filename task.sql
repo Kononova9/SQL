cb21ca2845f32994dc4de1049ccee5464c5ba080
Ksenia Kononova <117823922+Kononova9@users.noreply.github.com>
2023-06-09 17:16:56 +0300

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,13 +1,11 @@
---Найти информацию о всех контрактах, связанных с сотрудниками департамента
-      «Logistic». Вывести: contract_id, employee_name
+--Найти информацию о всех контрактах, связанных с сотрудниками департамента «Logistic». Вывести: contract_id, employee_name
 
      SELECT ex.contract_id, em.name FROM employees em
      JOIN department dp ON em.department_id = dp.id
      JOIN executor ex ON em.id = ex.tab_no
      WHERE dp.name = 'Logistic'
 
-     --Найти среднюю стоимость контрактов, заключенных 
-сотрудником IvanIvanov. Вывести: среднее значение amount
+     --Найти среднюю стоимость контрактов, заключенных  сотрудником IvanIvanov. Вывести: среднее значение amount
 
 SELECT ROUND(AVG(cn.amount), 2) FROM employees em
      JOIN executor ex ON em.id = ex.tab_no
@@ -22,7 +20,7 @@ ORDER BY COUNT(location)
 DESC
 LIMIT 1
 
---	Найти контракты одинаковой стоимости. Вывести count, amount
+--Найти контракты одинаковой стоимости. Вывести count, amount
 
 SELECT amount, COUNT(amount) FROM contract
 GROUP BY amount 
@@ -38,7 +36,7 @@ ORDER BY AVG(cn.amount)
 ASC
 LIMIT 1
 
---	Найти отдел, заключивший контрактов на наибольшую сумму. Вывести: department_name, sum
+--Найти отдел, заключивший контрактов на наибольшую сумму. Вывести: department_name, sum
 
 SELECT dp.name, SUM(amount) FROM employees em
      JOIN department dp ON em.department_id = dp.id
